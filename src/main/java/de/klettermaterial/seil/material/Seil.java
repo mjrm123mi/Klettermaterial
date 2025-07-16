9b25fb856886fa5beb0aec8c8e680c65b91ad403
Maryam Mirza <maryam.mirza@Student.HTW-Berlin.de>
2025-07-16 11:42:03 +0200

    Kleine Änderung

--- src/main/java/de/klettermaterial/seil/material/Seil.java
+++ src/main/java/de/klettermaterial/seil/material/Seil.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import java.time.LocalDate;
 
 /**
